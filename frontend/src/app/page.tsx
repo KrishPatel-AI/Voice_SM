fdf71885707c045568d93c34ded9ade0c8bd29dd
KrishPatel-AI <krish.work.ai@gmail.com>
2025-04-18 14:10:43 +0530

    Add Meta Data

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,6 +4,12 @@ import { Authentication } from "@/components/home/authentication";
 import { CallToAction } from "@/components/home/call-to-action";
 import { Market } from "@/components/home/market";
 import { Footer } from "@/components/layout/footer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "VoiceSM",
+  description: "Home Page of VoiceSM",
+};
 
 export default function Home() {
   return (
